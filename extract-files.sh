52290a12f3f7426b6eb53c0950aa7bddf77a3e42
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-23 15:02:48 +0100

    update vendor script to use platform-common
    Change-Id: Ib3055d0e89b96f6a3c2569b1d234b4e2ebaef4b7

--- extract-files.sh
+++ extract-files.sh
@@ -6,8 +6,11 @@ export LC_ALL=C
 FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
-export BOARDCONFIGVENDOR=true
+export BOARDCONFIGVENDOR=false
+export BOARD_VENDOR_PLATFORM=rhine
+export TARGET_BOARD_PLATFORM=msm8974
 
 ../common/extract-files.sh $@
-
 ../common/setup-makefiles.sh
+
+../${TARGET_BOARD_PLATFORM}-common/setup-makefiles.sh
